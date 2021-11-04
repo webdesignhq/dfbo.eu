f2ea7936fa9e0f9a44b5cb4cbb2ddbc8449c303a
j-polman <j.polman@webdesignhq.nl>
2021-11-04 13:32:31 +0100

    weglot

--- header.php
+++ header.php
@@ -44,8 +44,9 @@
 					<button class="menu-toggle btn"><i class="fas fa-bars"></i></button>
 					<nav id="site-navigation" class="main-navigation">
 						<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+						<div id="weglot_here"></div>
 					</nav>
-					<div id="weglot_here"></div>
+					
 				</div>
 				
 			</div>
